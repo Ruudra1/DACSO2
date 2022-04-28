5cc094c1ce2c545e82864950cdd06e310579c004
Ruudra Patel <ruudrapatel01@gmail.com>
2022-04-28 12:33:18 +0530

    Final Changes

---
+++ README.md
@@ -0,0 +1,30 @@
+# Automated Exploatory Data Analysis and Machine Learning
+
+This project is used to streamline the common steps used when looking at columns for data analysis. 
+
+There are 2 platforms within this app:
+1. A step-by-step EDA that highlights the numerical and categorical data used for feature analysis
+2. A streamlined ML platform that provides a baseline model for analysis as a citizen data scientist
+ 
+ Let’s look at what each of them can do:
+
+|EDA                          |ML                        
+|----------------|-----------------------------
+Granular understanding of individual features|Rapid data preprocessing           
+|In-depth insights on duplicated values | 11 different supervised classification models to evaluate the best metrics
+|Visual analysis on missing values | 10 Folds Cross Validation to find the best hyperparameters
+|Uni, bi and multivariate analysis of features         | 15 visual insights to understand the classification       
+|Precise breakdown on numeric, categorical and date-time features | Deploy a pipeline of the data preprocessing and model
+
+
+
+### Requirements:
+Streamlit <br/>
+Pycaret
+<br/>
+<br/>
+To run the platfrom locally, download the repository locally and type:
+#### streamlit run app.py
+<br/>
+<br/>
+ 
---
+++ intro.py
@@ -0,0 +1,36 @@
+import streamlit as st 
+from streamlit import *
+def hello():
+    st.write("## Aim")
+    st.write(" The aim of this project is to build a web based platform that helps to ease the data exploration and preprocessing steps and provide a smart and automated technique for exploratory analysis of the data. We also aim to predict possible values from the dataset by using various machine learning models and display it using different data visualization techniques.")
+    st.write("")
+    st.write("## About the project")
+    st.write("A lot of times, getting a high level analysis takes time to code and develop, and generally follows a linear path. This project was designed to streamline that process and eventually build a baseline machine learning model as a citizen data scientist. The fun part of this project is that it provides some toy datasets to get a feel of the platform.You can also upload your own datasets to get some insights from.")
+    st.write("")
+    st.write("## References")
+    st.write("")
+    st.write("**1. The Essential Toolbox for Data Cleaning**  - https://towardsdatascience.com/the-simple-yet-practical-data-cleaning-codes-ad27c4ce0a38")
+    st.write("**2. Code snippets from CHRIS ALBON** - https://chrisalbon.com/")
+    st.write("**3. Data Cleaning with Python** - https://towardsdatascience.com/data-cleaning-in-python-the-ultimate-guide-2020-c63b88bf0a0d")
+    st.write("**4. EDA for Feature Selection in ML** - http://services.google.com/fh/files/misc/exploratory_data_analysis_for_feature_selection_in_machine_learning.pdf")
+    st.write("**5. Pythonic Data Cleaning With Pandas and NumPy** - https://realpython.com/python-data-cleaning-numpy-pandas/")
+    st.write("")
+    st.write("")
+    st.write("### - Ruudra Patel, Simran Jindal and Jash Shah")
+
+
+if __name__ == "__main__":
+    hello()
+
+
+
+
+
+
+
+    # st.write("**6. Tour of Evaluation Metrics for Imbalanced Classification** - https://machinelearningmastery.com/tour-of-evaluation-metrics-for-imbalanced-classification/")
+    # st.write("**7. A Starter Pack to EDA** - https://towardsdatascience.com/a-starter-pack-to-exploratory-data-analysis-with-python-pandas-seaborn-and-scikit-learn-a77889485baf")
+    # st.write("**8. Building Web applications in Streamlit** - https://awesome-streamlit.readthedocs.io/en/latest/")
+    # st.write("**9. Utilizing Machine Learning Models through Pycaret** - https://pycaret.org/classification/")
+    # st.write("**10. Deploying Streamlit on Heroku** - https://towardsdatascience.com/deploy-streamlit-on-heroku-9c87798d2088")
+    # st.write("**11. Bulding Github Pages with Jekyll themes** - https://github.com/pages-themes/midnight")
